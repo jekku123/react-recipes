a2c260b8de50b963166abd9cfe173862adef50d0
Jesse Manninen <jesse.manninen@gmail.com>
2023-05-17 21:39:32 +0300

    logics

--- src/pages/RecipeInfo/index.js
+++ src/pages/RecipeInfo/index.js
@@ -2,7 +2,6 @@
 import { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import axios from 'axios';
-import { findFlagUrlByCountryName } from 'country-flags-svg';
 
 const RecipeInfo = () => {
   const [recipe, setRecipe] = useState(null);
