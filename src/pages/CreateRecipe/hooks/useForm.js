0c9e647cc9b9231985ca25d97363b3d6fdc02335
Jesse Manninen <jesse.manninen@gmail.com>
2023-05-20 05:03:03 +0300

    validation / error handling

--- src/pages/CreateRecipe/hooks/useForm.js
+++ src/pages/CreateRecipe/hooks/useForm.js
@@ -4,69 +4,207 @@ import { v4 as uuid } from 'uuid';
 // Fun :D <3
 
 const useForm = (init, submit) => {
-  const [formData, setFormData] = useState(init);
+    const [formData, setFormData] = useState(init);
+    const [errors, setErrors] = useState(init);
 
-  const removeObjectFromArray = (e, objArrayIdentifier) => {
-    const id = e.target.dataset.idx;
-    setFormData((prevData) => ({
-      ...prevData,
-      [objArrayIdentifier]: prevData[objArrayIdentifier].filter(
-        (obj) => id !== obj.id
-      ),
-    }));
-  };
-
-  const insertObjectToArray = (objArrayIdentifier, fields) => {
-    const newField = fields.reduce(
-      (acc, curr) => ({ ...acc, id: uuid(), [curr]: '' }),
-      {}
-    );
-    setFormData((prevData) => ({
-      ...prevData,
-      [objArrayIdentifier]: [...prevData[objArrayIdentifier], newField],
-    }));
-  };
-
-  const handleFormChanges = useCallback(
-    (e, objArrayIdentifier, arrayObjKey) => {
-      const { name, value, dataset } = e.target;
-      const id = dataset.idx;
-      if (name === arrayObjKey) {
+    const removeObjectFromArray = (e, objArrayIdentifier) => {
+        const id = e.target.dataset.idx;
         setFormData((prevData) => ({
-          ...prevData,
-          [objArrayIdentifier]: prevData[objArrayIdentifier].map((obj) => {
-            if (obj.id === id) {
-              return {
-                ...obj,
-                [name]: value,
-              };
+            ...prevData,
+            [objArrayIdentifier]: prevData[objArrayIdentifier].filter(
+                (obj) => id !== obj.id
+            ),
+        }));
+    };
+
+    const insertObjectToArray = (objArrayIdentifier, fields) => {
+        const newField = fields.reduce(
+            (acc, curr) => ({ ...acc, id: uuid(), [curr]: '' }),
+            {}
+        );
+        setFormData((prevData) => ({
+            ...prevData,
+            [objArrayIdentifier]: [...prevData[objArrayIdentifier], newField],
+        }));
+    };
+
+    const handleFormChanges = useCallback(
+        (e, objArrayIdentifier, arrayObjKey) => {
+            const { name, value, dataset } = e.target;
+            const id = dataset.idx;
+            validate(name, value);
+            if (name === arrayObjKey) {
+                setFormData((prevData) => ({
+                    ...prevData,
+                    [objArrayIdentifier]: prevData[objArrayIdentifier].map(
+                        (obj) => {
+                            if (obj.id === id) {
+                                return {
+                                    ...obj,
+                                    [name]: value,
+                                };
+                            }
+                            return obj;
+                        }
+                    ),
+                }));
+            } else {
+                setFormData((prevState) => ({
+                    ...prevState,
+                    [name]: value,
+                }));
             }
-            return obj;
-          }),
-        }));
-      } else {
-        setFormData((prevState) => ({
-          ...prevState,
-          [name]: value,
-        }));
-      }
-    },
-    []
-  );
+        },
+        []
+    );
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    submit(formData);
-    setFormData(init);
-  };
+    const validate = (name, value) => {
+        switch (name) {
+            case 'name':
+                if (value.length < 3) {
+                    setErrors((prevErrors) => {
+                        return {
+                            ...prevErrors,
+                            name: 'Name must be atleast 3 characters',
+                        };
+                    });
+                } else {
+                    setErrors((prevErrors) => {
+                        return {
+                            ...prevErrors,
+                            name: '',
+                        };
+                    });
+                }
+                break;
 
-  return {
-    formData,
-    handleFormChanges,
-    removeObjectFromArray,
-    insertObjectToArray,
-    handleSubmit,
-  };
+            case 'author':
+                if (value.length < 3) {
+                    setErrors((prevErrors) => {
+                        return {
+                            ...prevErrors,
+                            author: 'Name must be atleast 3 characters',
+                        };
+                    });
+                } else {
+                    setErrors((prevErrors) => {
+                        return {
+                            ...prevErrors,
+                            author: '',
+                        };
+                    });
+                }
+                break;
+
+            case 'country':
+                if (value.length < 3) {
+                    setErrors((prevErrors) => {
+                        return {
+                            ...prevErrors,
+                            country: 'Name must be atleast 3 characters',
+                        };
+                    });
+                } else {
+                    setErrors((prevErrors) => {
+                        return {
+                            ...prevErrors,
+                            country: '',
+                        };
+                    });
+                }
+                break;
+
+            case 'description':
+                if (value.length < 3) {
+                    setErrors((prevErrors) => {
+                        return {
+                            ...prevErrors,
+                            description: 'Name must be atleast 3 characters',
+                        };
+                    });
+                } else {
+                    setErrors((prevErrors) => {
+                        return {
+                            ...prevErrors,
+                            description: '',
+                        };
+                    });
+                }
+                break;
+
+            case 'imageUrl':
+                if (value.length < 3) {
+                    setErrors((prevErrors) => {
+                        return {
+                            ...prevErrors,
+                            imageUrl: 'Name must be atleast 3 characters',
+                        };
+                    });
+                } else {
+                    setErrors((prevErrors) => {
+                        return {
+                            ...prevErrors,
+                            imageUrl: '',
+                        };
+                    });
+                }
+                break;
+
+            case 'ingredients':
+                if (value.length < 3) {
+                    setErrors((prevErrors) => {
+                        return {
+                            ...prevErrors,
+                            ingredients: 'Name must be atleast 3 characters',
+                        };
+                    });
+                } else {
+                    setErrors((prevErrors) => {
+                        return {
+                            ...prevErrors,
+                            ingredients: '',
+                        };
+                    });
+                }
+                break;
+
+            case 'instructions':
+                if (value.length < 3) {
+                    setErrors((prevErrors) => {
+                        return {
+                            ...prevErrors,
+                            instructions: 'Name must be atleast 3 characters',
+                        };
+                    });
+                } else {
+                    setErrors((prevErrors) => {
+                        return {
+                            ...prevErrors,
+                            instructions: '',
+                        };
+                    });
+                }
+                break;
+
+            default:
+                break;
+        }
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        submit(formData);
+        setFormData(init);
+    };
+
+    return {
+        formData,
+        handleFormChanges,
+        removeObjectFromArray,
+        insertObjectToArray,
+        handleSubmit,
+        errors,
+    };
 };
 
 export default useForm;
--- src/pages/CreateRecipe/index.js
+++ src/pages/CreateRecipe/index.js
@@ -8,83 +8,97 @@ import TextArea from './components/TextArea';
 import Button from './components/Button';
 
 const initialValues = {
-  name: '',
-  author: '',
-  country: '',
-  description: '',
-  imageUrl: '',
-  ingredients: [{ id: uuid(), quantity: '', ingredient: '' }],
-  instructions: '',
+    name: '',
+    author: '',
+    country: '',
+    description: '',
+    imageUrl: '',
+    ingredients: [{ id: uuid(), quantity: '', ingredient: '' }],
+    instructions: '',
 };
 
 const CreateRecipe = () => {
-  const {
-    formData,
-    handleFormChanges,
-    removeObjectFromArray,
-    insertObjectToArray,
-    handleSubmit,
-  } = useForm(initialValues, () => submit());
+    const {
+        formData,
+        handleFormChanges,
+        removeObjectFromArray,
+        insertObjectToArray,
+        handleSubmit,
+        errors,
+    } = useForm(initialValues, () => submit());
 
-  const submit = async () => {
-    try {
-      await axios.post('http://localhost:3001/recipes', {
-        id: uuid(),
-        ...formData,
-      });
-      window.alert('Recipe posted');
-    } catch (err) {
-      console.log('Error: ' + err);
-    }
-  };
+    const submit = async () => {
+        try {
+            await axios.post('http://localhost:3001/recipes', {
+                id: uuid(),
+                ...formData,
+            });
+            alert('Recipe posted');
+        } catch (err) {
+            console.log('Error: ' + err);
+        }
+    };
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <Input
-        label='Name'
-        name='name'
-        handler={handleFormChanges}
-        value={formData.name}
-      />
-      <Input
-        label='Author'
-        name='author'
-        handler={handleFormChanges}
-        value={formData.author}
-      />
-      <Select
-        label='Choose a country'
-        country={formData.country}
-        handler={handleFormChanges}
-        value={formData.country}
-      />
-      <Input
-        label='Description'
-        name='description'
-        handler={handleFormChanges}
-        value={formData.description}
-      />
-      <Input
-        label='Image'
-        name='imageUrl'
-        handler={handleFormChanges}
-        value={formData.imageUrl}
-      />
-      <Ingredients
-        ingredients={formData.ingredients}
-        handleFormChanges={handleFormChanges}
-        insertObjectToArray={insertObjectToArray}
-        removeObjectFromArray={removeObjectFromArray}
-      />
-      <TextArea
-        label='Instructions'
-        name='instructions'
-        handleFormChanges={handleFormChanges}
-        value={formData.instructions}
-      />
-      <Button text='Add recipe' />
-    </form>
-  );
+    return (
+        <form onSubmit={handleSubmit}>
+            <Input
+                label='Name'
+                name='name'
+                handler={handleFormChanges}
+                value={formData.name}
+            />
+            <span>{errors?.name}</span>
+            <Input
+                label='Author'
+                name='author'
+                handler={handleFormChanges}
+                value={formData.author}
+            />
+            <span>{errors?.author}</span>
+
+            <Select
+                label='Choose a country'
+                country={formData.country}
+                handler={handleFormChanges}
+                value={formData.country}
+            />
+            <span>{errors?.country}</span>
+
+            <Input
+                label='Description'
+                name='description'
+                handler={handleFormChanges}
+                value={formData.description}
+            />
+            <span>{errors?.description}</span>
+
+            <Input
+                label='Image'
+                name='imageUrl'
+                handler={handleFormChanges}
+                value={formData.imageUrl}
+            />
+            <span>{errors?.imageUrl}</span>
+
+            <Ingredients
+                ingredients={formData.ingredients}
+                handleFormChanges={handleFormChanges}
+                insertObjectToArray={insertObjectToArray}
+                removeObjectFromArray={removeObjectFromArray}
+            />
+            <span>{errors?.name}</span>
+
+            <TextArea
+                label='Instructions'
+                name='instructions'
+                handleFormChanges={handleFormChanges}
+                value={formData.instructions}
+            />
+            <span>{errors?.instructions}</span>
+
+            <Button text='Add recipe' />
+        </form>
+    );
 };
 
 export default CreateRecipe;
